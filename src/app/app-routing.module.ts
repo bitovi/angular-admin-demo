c51436b9bebaf284707b190c085564e305a3358e
Kyle Nazario <knazario@bitovi.com>
2021-05-24 14:41:30 -0600

    Simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: HomePageComponent },
+  { path: 'reset-password', component: ResetPasswordPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
