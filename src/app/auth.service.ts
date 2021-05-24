36b0cf8c9fdcd4e7743b996ec3cb5bec17ee4c3c
Kyle Nazario <knazario@bitovi.com>
2021-05-24 15:06:21 -0600

    Auth data service tracks logins

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+type AuthData = {
+  username: string;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  public loggedIn: Observable<boolean>;
+
+  public username: Observable<string | undefined>;
+
+  private _authData = new ReplaySubject<AuthData | undefined>(1);
+
+  constructor() {
+    this.loggedIn = this._authData.pipe(
+      map((authData) => {
+        const usernameLength = authData?.username.length ?? 0;
+        return usernameLength > 0;
+      })
+    );
+
+    this.username = this._authData.pipe(map((authData) => authData?.username));
+  }
+
+  logIn(username: string, password: string) {
+    this._authData.next({ username });
+  }
 }
