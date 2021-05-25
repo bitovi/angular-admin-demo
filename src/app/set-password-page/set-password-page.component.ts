513aa678b0a025495d18b3ccfbc88d2421a52f77
Kyle Nazario <knazario@bitovi.com>
2021-05-25 13:03:25 -0600

    Password form

--- src/app/set-password-page/set-password-page.component.ts
+++ src/app/set-password-page/set-password-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-set-password-page',
   templateUrl: './set-password-page.component.html',
   styleUrls: ['./set-password-page.component.scss'],
 })
-export class SetPasswordPageComponent {
-  constructor() {}
+export class SetPasswordPageComponent implements OnInit {
+  passwordForm = new FormGroup({
+    password: new FormControl('', Validators.required),
+    repeated: new FormControl('', Validators.required),
+  });
+
+  ngOnInit(): void {
+    const matchValidatorFactory = (): ValidatorFn => {
+      return (control: AbstractControl): ValidationErrors | null => {
+        const passwordControl = control.get('password') as FormControl;
+        const repeatedControl = control.get('repeated') as FormControl;
+        const match = passwordControl.value === repeatedControl.value;
+        return match ? null : { passwordMismatch: true };
+      };
+    };
+    this.passwordForm.setValidators(matchValidatorFactory);
+  }
 }
