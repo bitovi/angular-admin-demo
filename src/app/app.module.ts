8f804e3b8f01f749bdc577f610dee9f6dc563bdb
Kyle Nazario <knazario@bitovi.com>
2021-05-25 09:46:26 -0600

    Initial form group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AuthService } from './auth.service';
 import { HeaderComponent } from './header/header.component';
@@ -24,7 +25,14 @@ import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
     ResetPasswordPageComponent,
     SidebarNavComponent,
   ],
-  imports: [ButtonModule, CardModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule],
+  imports: [
+    ButtonModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule,
+    ReactiveFormsModule,
+  ],
   providers: [AuthService],
   bootstrap: [AppComponent],
 })
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  public readonly loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
   constructor() {}
 }
