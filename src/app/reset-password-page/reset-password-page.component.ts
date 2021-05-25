5fd8a7fa53e0b79be67bb1671ee9fbfd88653ccd
Kyle Nazario <knazario@bitovi.com>
2021-05-25 12:53:04 -0600

    Reset password form

--- src/app/reset-password-page/reset-password-page.component.ts
+++ src/app/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { filter, take } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password-page.component.scss'],
 })
 export class ResetPasswordPageComponent {
-  constructor() {}
+  resetForm: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.resetForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+    });
+
+    authService.username
+      .pipe(
+        filter((username) => Boolean(username)),
+        take(1)
+      )
+      .subscribe((username) => {
+        this.resetForm.get('username')?.patchValue(username);
+      });
+  }
+
+  submit(): void {
+    this.router.navigate(['/', 'set-password'], { queryParams: { username: this.resetForm.value.username } });
+  }
+
+  shouldDisplayRequiredMessage(formControl: AbstractControl): boolean {
+    return formControl.touched && formControl.invalid;
+  }
 }
