6ee6da5a3c3f32b25fcfddb91792132d6a23a0d9
Kyle Nazario <knazario@bitovi.com>
2021-06-02 10:23:56 -0600

    Imports PrimeNG components used on demo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipsModule } from 'primeng/chips';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 import { AuthService } from './auth.service';
 import { CountryService } from './countryservice';
@@ -39,11 +51,23 @@ import { InputDemoComponent } from './input-demo-page/inputdemo.component';
   ],
   imports: [
     AppRoutingModule,
+    AutoCompleteModule,
     BrowserAnimationsModule,
     BrowserModule,
     ButtonModule,
+    CalendarModule,
+    CheckboxModule,
+    ChipsModule,
+    DropdownModule,
+    FormsModule,
+    InputNumberModule,
     InputTextModule,
+    KnobModule,
+    ListboxModule,
+    MultiSelectModule,
+    RatingModule,
     ReactiveFormsModule,
+    SelectButtonModule,
   ],
   providers: [AppBreadcrumbService, AuthService, CountryService],
   bootstrap: [AppComponent],
